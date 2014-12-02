30df0a80b711913ad0b0eaf877eb6e4effaca60b
PancakePatrol <seg2@rice.edu>
2014-12-02 13:05:40 -0600

    Minor format changes
    formatting

--- KeypadProject/KeypadProject.ino
+++ KeypadProject/KeypadProject.ino
@@ -3,14 +3,16 @@
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
-    {'1','2','3'},
-    {'4','5','6'},
-    {'7','8','9'},
-    {'*','0','#'}
+  {'1','2','3'  },
+  {'4','5','6'  },
+  {'7','8','9'  },
+  {'*','0','#'  }
 };
 
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {
+  5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {
+  8, 7, 6}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 byte ledPin = 13; 
@@ -24,199 +26,201 @@ int WRONG = 12;
 
 char code[] = {0,0,0,0}; // Will serve as arrary that carries passcode
 char cod[] = {0,0,0,0};  // Will serve as array that carries attempted input
+
 void setup(){
-    pinMode(OHYEAH, OUTPUT);
-    
-    digitalWrite(OHYEAH, HIGH);
-    digitalWrite(9, HIGH);
-    pinMode(9, OUTPUT);
-    Serial.begin(9600);
-    delay(1000);
-    pinMode(ledPin, OUTPUT);              // Sets the digital pin as output.
-    digitalWrite(ledPin, HIGH);           // Turn the LED on.
-    ledPin_state = digitalRead(ledPin);   // Store initial LED state. HIGH when LED is on.
-    keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
-    Serial.println("Set Up Passcode");
-    delay(1000);
-    Serial.println("Input first key");
-    code[0] = keypad.waitForKey();
-    Serial.println(code[0]);
-    Serial.println("Input second key");
-    code[1] = keypad.waitForKey();
-      Serial.println(code[1]);
-    Serial.println("Input third key");
-    code[2] = keypad.waitForKey();
-      Serial.println(code[2]);
-    Serial.println("Input fourth key");
-    code[3] = keypad.waitForKey();
-      Serial.println(code[3]);
-      Serial.println("Passcode Set!");
+  pinMode(OHYEAH, OUTPUT);
+
+  digitalWrite(OHYEAH, HIGH);
+  digitalWrite(9, HIGH);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+  delay(1000);
+  pinMode(ledPin, OUTPUT);              // Sets the digital pin as output.
+  digitalWrite(ledPin, HIGH);           // Turn the LED on.
+  ledPin_state = digitalRead(ledPin);   // Store initial LED state. HIGH when LED is on.
+  keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
+  Serial.println("Set Up Passcode");
+  delay(1000);
+  Serial.println("Input first key");
+  code[0] = keypad.waitForKey();
+  Serial.println(code[0]);
+  Serial.println("Input second key");
+  code[1] = keypad.waitForKey();
+  Serial.println(code[1]);
+  Serial.println("Input third key");
+  code[2] = keypad.waitForKey();
+  Serial.println(code[2]);
+  Serial.println("Input fourth key");
+  code[3] = keypad.waitForKey();
+  Serial.println(code[3]);
+  Serial.println("Passcode Set!");
 }
 
 void loop(){
-    digitalWrite(OHYEAH, LOW);
-    char key = keypad.getKey();
-    pinMode(WRONG, OUTPUT);
-    //digitalWrite(WRONG, HIGH);
-    if (key == '*' && Attempt < 3){
-          Serial.println("Begin Passcode Input!");
-          blink();
-          Serial.println("Input first key");
-          cod[0] = keypad.waitForKey();
-          Serial.println(cod[0]);
-          blink();
-          Serial.println("Input next key");
-          cod[1] = keypad.waitForKey();
-          Serial.println(cod[1]);
-          blink();
-          Serial.println("Input next key");
-          cod[2] = keypad.waitForKey();
-          Serial.println(cod[2]);
-          blink();
-          Serial.println("Input next key");
-          cod[3] = keypad.waitForKey();
-          Serial.println(cod[3]);
-          
-               if (code[0] == cod[0] && code[1] == cod[1] && code[2] == cod[2] && code[3] == cod[3]){
-                Serial.println("Match! Opening Door!");
-                Attempt = 0;
-                //digitalWrite(ledPin,!digitalRead(ledPin));
-                digitalWrite(OHYEAH, HIGH);
-                motor();
-                //delay(5000);
-                digitalWrite(OHYEAH, LOW);
-                }
-                
-                else{
-                     Serial.println("No Match!");
-                     digitalWrite(WRONG, HIGH);
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     Attempt = Attempt + 1;
-                     digitalWrite(WRONG, LOW);
-                 }
-    }
-    else if(Attempt == 3){
-      Serial.println("LOCK OUT!");
-      digitalWrite(WRONG, HIGH);
-                           delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     Attempt = 0;
-                   digitalWrite(WRONG, LOW);  
+  digitalWrite(OHYEAH, LOW);
+  char key = keypad.getKey();
+  pinMode(WRONG, OUTPUT);
+  //digitalWrite(WRONG, HIGH);
+  if (key == '*' && Attempt < 3){
+    Serial.println("Begin Passcode Input!");
+    blink();
+    Serial.println("Input first key");
+    cod[0] = keypad.waitForKey();
+    Serial.println(cod[0]);
+    blink();
+    Serial.println("Input next key");
+    cod[1] = keypad.waitForKey();
+    Serial.println(cod[1]);
+    blink();
+    Serial.println("Input next key");
+    cod[2] = keypad.waitForKey();
+    Serial.println(cod[2]);
+    blink();
+    Serial.println("Input next key");
+    cod[3] = keypad.waitForKey();
+    Serial.println(cod[3]);
+
+    if (code[0] == cod[0] && code[1] == cod[1] && code[2] == cod[2] && code[3] == cod[3]){
+      Serial.println("Match! Opening Door!");
+      Attempt = 0;
+      //digitalWrite(ledPin,!digitalRead(ledPin));
+      digitalWrite(OHYEAH, HIGH);
+      motor();
+      //delay(5000);
+      digitalWrite(OHYEAH, LOW);
     }
 
-    if (key == '#'){
-          Serial.println("Begin Passcode Input!");
-          blink();
-          Serial.println("Input first key");
-          cod[0] = keypad.waitForKey();
-          Serial.println(cod[0]);
-          blink();
-          Serial.println("Input next key");
-          cod[1] = keypad.waitForKey();
-          Serial.println(cod[1]);
-          blink();
-          Serial.println("Input next key");
-          cod[2] = keypad.waitForKey();
-          Serial.println(cod[2]);
-          blink();
-          Serial.println("Input next key");
-          cod[3] = keypad.waitForKey();
-          Serial.println(cod[3]);
-          
-               if (code[0] == cod[0] && code[1] == cod[1] && code[2] == cod[2] && code[3] == cod[3]){
-                Serial.println("Match! Resetting passcode...");
-                digitalWrite(ledPin,!digitalRead(ledPin));
-                digitalWrite(OHYEAH, HIGH);
-                delay(1000);
-                digitalWrite(OHYEAH, LOW);
- 
-                
-                    delay(1000);
-                    Serial.println("Input first key");
-                    code[0] = keypad.waitForKey();
-                    Serial.println(code[0]);
-                    blink();
-                    Serial.println("Input second key");
-                    code[1] = keypad.waitForKey();
-                    Serial.println(code[1]);
-                    blink();
-                    Serial.println("Input third key");
-                    code[2] = keypad.waitForKey();
-                    Serial.println(code[2]);
-                    blink();
-                    Serial.println("Input fourth key");
-                    code[3] = keypad.waitForKey();
-                    Serial.println(code[3]);
-                    blink();
-                    Serial.println("Passcode Set!");
-                
-                }
-                
-                else{
-                     Serial.println("No Match!");
-                     digitalWrite(WRONG, HIGH);
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     delay(100);
-                     blink();
-                     Attempt = Attempt + 1;
-                     digitalWrite(WRONG, LOW);
-                 }    
-     }
+    else{
+      Serial.println("No Match!");
+      digitalWrite(WRONG, HIGH);
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      Attempt = Attempt + 1;
+      digitalWrite(WRONG, LOW);
+    }
+  }
+  else if(Attempt == 3){
+    Serial.println("LOCK OUT!");
+    digitalWrite(WRONG, HIGH);
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    delay(100);
+    blink();
+    Attempt = 0;
+    digitalWrite(WRONG, LOW);  
+  }
+
+  if (key == '#'){
+    Serial.println("Begin Passcode Input!");
+    blink();
+    Serial.println("Input first key");
+    cod[0] = keypad.waitForKey();
+    Serial.println(cod[0]);
+    blink();
+    Serial.println("Input next key");
+    cod[1] = keypad.waitForKey();
+    Serial.println(cod[1]);
+    blink();
+    Serial.println("Input next key");
+    cod[2] = keypad.waitForKey();
+    Serial.println(cod[2]);
+    blink();
+    Serial.println("Input next key");
+    cod[3] = keypad.waitForKey();
+    Serial.println(cod[3]);
+
+    if (code[0] == cod[0] && code[1] == cod[1] && code[2] == cod[2] && code[3] == cod[3]){
+      Serial.println("Match! Resetting passcode...");
+      digitalWrite(ledPin,!digitalRead(ledPin));
+      digitalWrite(OHYEAH, HIGH);
+      delay(1000);
+      digitalWrite(OHYEAH, LOW);
+
+
+      delay(1000);
+      Serial.println("Input first key");
+      code[0] = keypad.waitForKey();
+      Serial.println(code[0]);
+      blink();
+      Serial.println("Input second key");
+      code[1] = keypad.waitForKey();
+      Serial.println(code[1]);
+      blink();
+      Serial.println("Input third key");
+      code[2] = keypad.waitForKey();
+      Serial.println(code[2]);
+      blink();
+      Serial.println("Input fourth key");
+      code[3] = keypad.waitForKey();
+      Serial.println(code[3]);
+      blink();
+      Serial.println("Passcode Set!");
+
+    }
+
+    else{
+      Serial.println("No Match!");
+      digitalWrite(WRONG, HIGH);
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      delay(100);
+      blink();
+      Attempt = Attempt + 1;
+      digitalWrite(WRONG, LOW);
+    }    
+  }
 }
 
 
 void blink() {
-            digitalWrite(OHYEAH, HIGH);
-            delay(500);
-            digitalWrite(OHYEAH, LOW);
+  digitalWrite(OHYEAH, HIGH);
+  delay(500);
+  digitalWrite(OHYEAH, LOW);
 }
 
 
 void motor() {
-    digitalWrite(9, HIGH);
-    delay(1000);
-    digitalWrite(9, LOW);
-    delay(1000);
-    digitalWrite(9, HIGH);
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+  delay(1000);
+  digitalWrite(9, HIGH);
 }
 
 
 void keypadEvent(KeypadEvent key){
-    switch (keypad.getState()){
-    }
+  switch (keypad.getState()){
+  }
 
 }
+
